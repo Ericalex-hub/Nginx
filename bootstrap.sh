eda4fe0ef8f22017f50bafd45018d01c4081ac34
Ericalex-hub <ealdsan503@g.educaand.es>
2025-11-25 19:29:30 +0100

    Creación de carpeta y clonación de repositorio

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
 sudo apt update
 sudo apt install nginx
 systemctl status nginx
+sudo mkdir -p /var/www/example.test/html
+cd /var/www/example.test/html
+sudo git clone https://github.com/cloudacademy/static-website-example
